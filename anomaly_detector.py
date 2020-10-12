735910a98f8fe3bfc7e1334353c38abe1788fc2b
ledbagholberton <789@holbertonschool.com>
2020-10-12 17:43:59 -0500

    Directories

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 """
-Function Anomaly Detection
+Function Anomaly Detector
 Based on the trained model anomaly_autoencoder.mh5
 Inputs: path of directory with Batch of pictures.
 Outputs: Dictionary with names of files and "Yes / No" for Anomaly
 """
+from PIL import Image
+import glob
+import os
+
+
+def anomaly_detector(path_source, path_target, path_model_anomaly):
+    """Function Anomaly Detector"""
+    source = path_source
+    # All jpegs are in this folder
+    imList = glob.glob(source + '*.jpg')
+    # Reading all images with .jpg
+    
