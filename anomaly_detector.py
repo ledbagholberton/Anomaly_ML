afe819d73370890de92665378c0854da3bba6097
ledbagholberton <789@holbertonschool.com>
2020-10-12 15:43:05 -0500

    First commit - Basic files

---
+++ anomaly_detector.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Function Anomaly Detection
+Based on the trained model anomaly_autoencoder.mh5
+Inputs: path of directory with Batch of pictures.
+Outputs: Dictionary with names of files and "Yes / No" for Anomaly
+"""
---
+++ preprocessing_noising.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Function Preprocessing_noising
+Take a small dataset of pictures and generates syntethic data
+based on flip library.
+Inputs: path of directory with small dataset.
+        path of directory target
+Outputs: syntethic data in directory target
+"""
