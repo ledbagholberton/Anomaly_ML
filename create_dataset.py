6592adc26a2e958ff9f45fc12e9206a2f2a3add0
Abdel Giovanny Perez <47123404+ledbagholberton@users.noreply.github.com>
2020-11-09 11:28:07 -0500

    Delete create_dataset.py
    Redundant funnction

--- create_dataset.py
+++
@@ -1,8 +0,0 @@
-from generate_data import generate_data
-
-generate_data(n_samples=1,
-              n_objects=1,
-              backgronds_pattern="data/backgrounds/*",
-              objects_pattern="data/objects/*",
-              output_dir="data/output",
-              show=False)
