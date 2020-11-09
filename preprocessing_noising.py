93150cd1280c3c974dd37b3b1bd67db19ba409a9
Abdel Giovanny Perez <47123404+ledbagholberton@users.noreply.github.com>
2020-11-09 13:22:17 -0500

    Delete preprocessing_noising.py
    notrequired

--- preprocessing_noising.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Function Preprocessing_noising
-Take a small dataset of pictures and generates syntethic data
-based on flip library.
-Inputs: path of directory with small dataset.
-        path of directory target
-Outputs: syntethic data in directory target
-"""
