834fcd2ba373d3230d97cc7ba3ac9608588af565
Heimer Rojas <732@holbertonschool.com>
2020-11-11 15:31:16 +0000

    autoencoder functions added

--- data_load.py
+++ data_load.py
@@ -3,61 +3,66 @@ import glob
 import numpy as np
 import cv2
 
-def rescale(img, scale_percent):
-    """ resize images """
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    # resize image
-    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-def load_images(images_path, noise_path, samples=None, scale=None):
+def load_images(images_path, noise_path, samples=None,
+                out_shape=None,
+                train_percentage=0.95):
     """
-        - images_path is the path to a directory from which to load images without
-	noise
-	- noise_path is the path to a directory from which to load synthetic images
-        - samples is the number of synthetic images to use
-        - scale is the factor to scale the images, if it is None, it is ignored
+        - images_path is the path to a directory from which to load images
+                without noise
+        - noise_path is the path to a directory from wich to load noisy images
+        - samples: number of images to create dataset
+        - out_shape: shape of images to rescale
+
         All images are loaded in RGB format
-        Returns: x_data, x_noisy_data
-            x_data are the clean images
-            x_noisy_data are the synthetic images
+
+        Returns: x_data - images without noise
+                 x_noisy_data - noisy images
     """
-    print(noise_path)
-    backgnd_paths = glob.glob(images_path + "/*.jpg")
+    backgnd_paths = glob.glob(images_path + "/*.jpeg")
     noisy_img_paths = glob.glob(noise_path + "/*.jpg")
 
     backgnd_paths.sort()
     noisy_img_paths.sort()
 
-    print(backgnd_paths)
-    print(noisy_img_paths)
-
-    if samples != None:
+    if samples is not None:
         noisy_img_paths = noisy_img_paths[:samples]
-    print("loading backgrounds - clean images")
+
+    print("loading images without noise..", end='')
     x_dict = {}
     for i, path in enumerate(backgnd_paths):
-          img = cv2.imread(path)
-          img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-          if scale != None:
-              img = rescale(img, scale)
-          x_dict[path.split('/')[-1].split('.')[0]] = img
+        if i % 100 == 0:
+            print(".", end='')
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if out_shape is not None:
+            img = cv2.resize(img, out_shape, interpolation=cv2.INTER_AREA)
+            # img = rescale(img, out_shape)
+        x_dict[path.split('/')[-1].split('.')[0]] = img
+    print("")
     print("loading noisy images..", end='')
     x_data = []
     x_noisy_data = []
     for i, img_noisy in enumerate(noisy_img_paths):
-        print(".", end='')
-        name = img_noisy.split('/')[-1].split('_')[0]
+        if i % 100 == 0:
+            print(".", end='')
+        name = img_noisy.split('/')[-1].split('__')[0]
         img_noisy = cv2.imread(img_noisy)
         img_noisy = cv2.cvtColor(img_noisy, cv2.COLOR_BGR2RGB)
-        if scale != None:
-            img_noisy = rescale(img_noisy, scale)
+        if out_shape is not None:
+            img_noisy = cv2.resize(
+                img_noisy, out_shape, interpolation=cv2.INTER_AREA)
         x_noisy_data.append(img_noisy)
         x_data.append(x_dict[name])
     print("")
-
     x_data = np.stack(x_data, axis=0)
     x_noisy_data = np.stack(x_noisy_data, axis=0)
 
-    return x_data, x_noisy_data
+    train_samples = int(x_noisy_data.shape[0] * (train_percentage))
+
+    x_train = x_noisy_data[:train_samples]
+    y_train = x_data[:train_samples]
+    x_valid = x_noisy_data[train_samples:]
+    y_valid = x_data[train_samples:]
+
+    return (x_train, y_train), (x_valid, y_valid)
